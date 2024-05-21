749cbad4fe51c689b6ba8e30fac7d4a2c4dd5886
sauli <sauli.tarvainen@skillwell.fi>
2024-05-21 21:50:33 +0300

    add missing commas :)

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -8,15 +8,15 @@
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base",  sensor-bindings=<&inc_dec_kp PG_UP PG_DN>;) \
-MIRYOKU_X(EXTRA,  "Extra" ) \
-MIRYOKU_X(TAP,    "Tap"	  ) \
-MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(EXTRA,  "Extra", ) \
+MIRYOKU_X(TAP,    "Tap",   ) \
+MIRYOKU_X(BUTTON, "Button",) \
 MIRYOKU_X(NAV,    "Nav",   sensor-bindings=<&inc_dec_kp UP DOWN>;) \
-MIRYOKU_X(MOUSE,  "Mouse" ) \
+MIRYOKU_X(MOUSE,  "Mouse", ) \
 MIRYOKU_X(MEDIA,  "Media", sensor-bindings=<&inc_dec_kp C_VOL_DN C_VOL_UP>;) \
 MIRYOKU_X(NUM,    "Num",   sensor-bindings=<&inc_dec_kp C_BRIGHTNESS_INC C_BRIGHTNESS_DEC>;) \
 MIRYOKU_X(SYM,    "Sym",   sensor-bindings=<&inc_dec_kp LEFT RIGHT>;) \
-MIRYOKU_X(FUN,    "Fun"	  )
+MIRYOKU_X(FUN,    "Fun",   )
 
 #define U_BASE   0
 #define U_EXTRA  1
