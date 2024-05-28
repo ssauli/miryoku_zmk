845e2fc73f117e63006a9367d5292f3b4333232f
sauli <sauli.tarvainen@skillwell.fi>
2024-05-28 21:44:48 +0300

    fix

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -16,7 +16,7 @@ U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp 
 ) \
 XXX 	    K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
 XXX  	    K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-&to U_GAME  K20  K21  K22  K23  K24  XXX  K25  K26  K27  K28  K29  $to U_BASE \
+&to U_GAME  K20  K21  K22  K23  K24  XXX  K25  K26  K27  K28  K29  &to U_BASE \
      	         XXX  K32  K33  K34	      K35  K36  K37  XXX
 
 #define MIRYOKU_LAYER_LIST \
